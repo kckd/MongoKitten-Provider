981898140db58438ea9074152abab18de674d9d1
kckd <casey@kckd.org>
2018-07-08 12:38:15 -0700

    Add dependency for Node

--- Package.swift
+++ Package.swift
@@ -9,8 +9,9 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/OpenKitten/MongoKitten.git", from: "4.0.0"),
         .package(url: "https://github.com/vapor/vapor.git", from: "3.0.0"),
+        .package(url: "https://github.com/vapor-community/node.git", from: "2.1.0"),
     ],
     targets: [
-        .target(name: "MongoKittenProvider", dependencies: ["MongoKitten", "Vapor"])
+        .target(name: "MongoKittenProvider", dependencies: ["MongoKitten", "Vapor", "Node"])
         ]
 )
