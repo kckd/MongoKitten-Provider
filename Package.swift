d99762e928fd80c8b99ce5a84d21b802adf041af
kckd <casey@kckd.org>
2018-07-08 12:12:46 -0700

    Add products

--- Package.swift
+++ Package.swift
@@ -3,6 +3,9 @@ import PackageDescription
 
 let package = Package(
     name: "MongoKittenProvider",
+    products: [
+        .library(name: "MongoKittenProvider", targets: ["MongoKittenProvider"]),
+    ],
     dependencies: [
         .package(url: "https://github.com/OpenKitten/MongoKitten.git", from: "4.0.0"),
         .package(url: "https://github.com/vapor/vapor.git", from: "3.0.0"),
