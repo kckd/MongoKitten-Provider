589997dde5973360b77406bdc9664c82a5a86007
kckd <casey@kckd.org>
2018-07-08 12:01:52 -0700

    Add target to Package.swift

--- Package.swift
+++ Package.swift
@@ -6,5 +6,8 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/OpenKitten/MongoKitten.git", from: "4.0.0"),
         .package(url: "https://github.com/vapor/vapor.git", from: "3.0.0"),
-    ]
+    ],
+    targets: [
+        .target(name: "MongoKittenProvider", dependencies: ["MongoKitten"])
+        ]
 )
